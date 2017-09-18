f30198a783679aa957fe526a5d46aaefe5d2c116
jeloi <recycled_loops@hotmail.com>
2017-09-18 15:26:46 +0200

    Readme

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 Clone the repo and do ```npm install``` inside the directory
 
 ### Serve with hot reload at localhost:3000
-Then run ```npm run dev```and will be server on localhost:3000
+Then run ```npm run server```and will be server on localhost:3000
 
 ### Build for production with minification
 If you want to build for production ```npm run build```
--- server/routes/users.js
+++ server/routes/users.js
@@ -3,32 +3,6 @@ import validateInput from '../shared/validations/signup';
 
 let router = express.Router();
 
-// function validateInput(data) {
-//
-//   let errors = {};
-//
-//   if (Validator.isEmpty(data.firstname)) {
-//     errors.firstname = 'This field is required';
-//   }
-//   if (Validator.isEmpty(data.lastname)) {
-//     errors.lastname = 'This field is required';
-//   }
-//   if (Validator.isEmpty(data.username)) {
-//     errors.username = 'This field is required';
-//   }
-//   if (Validator.isEmpty(data.password)) {
-//     errors.password = 'This field is required';
-//   }
-//   if (Validator.isEmpty(data.email)) {
-//     errors.email = 'This field is required';
-//   }
-//   if (!Validator.isEmail(data.email)) {
-//     errors.email = 'Email is invalid';
-//   }
-//
-//   return {errors, isValid: isEmpty(errors)}
-// }
-
 router.post('/', (req, res) => {
   const {errors, isValid} = validateInput(req.body);
 
